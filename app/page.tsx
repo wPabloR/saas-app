77f3bac483127cc5d2253fd86f3fceae25076100
wPabloR <ricotep0@gmail.com>
2026-01-02 12:56:43 +0100

    add some pages ui

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,52 @@
-import { Button } from '@/components/ui/button'
-import React from 'react'
+import CompanionCard from "@/components/CompanionCard";
+import CompanionList from "@/components/CompanionList";
+import CTA from "@/components/CTA";
+import { Button } from "@/components/ui/button";
+import { recentSessions } from "@/constants";
+import React from "react";
 
 const Page = () => {
   return (
-    <div>
-      <h1 className='text-2xl underline'>Welcome to my SaaS App</h1>
-      <Button>
-        Im a button
-      </Button>
-    </div>
-  )
-}
+    <main>
+      <h1>Popular Companions</h1>
 
-export default Page
+      <section className="home-section">
+        <CompanionCard
+          id="123"
+          name="Neura the Brainy Explorer"
+          topic="Neural Network of the Brain"
+          subject="science"
+          duration={45}
+          color="#ffda6e"
+        />
+        <CompanionCard
+          id="223"
+          name="Counsty the Number Wizard"
+          topic="Derivatives & Integrals"
+          subject="maths"
+          duration={30}
+          color="#e5d0ff"
+        />
+        <CompanionCard
+          id="789"
+          name="Verba the Vocabulary Builder"
+          topic="Language"
+          subject="English Literature"
+          duration={30}
+          color="#bde7ff"
+        />
+      </section>
+
+      <section className="home-section">
+        <CompanionList 
+          title="Reciently completed sessions"
+          companions={recentSessions}
+          classNames="w-2/3 max-lg:w-full" 
+        />
+        <CTA />
+      </section>
+    </main>
+  );
+};
+
+export default Page;
